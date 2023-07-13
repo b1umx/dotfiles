dba7aa25e217ae3bf30cfc0cf2a4db4972cfd40b
b1umx <b1umx@yandex.ru>
2023-07-13 21:15:01 +0300

    cnahge tagbar key binding

--- nvim/lua/packages/tagbar-setup.lua
+++ nvim/lua/packages/tagbar-setup.lua
@@ -1,2 +1,2 @@
-vim.keymap.set('n', '<leader>t', '<Cmd>TagbarToggle<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<F8>', '<Cmd>TagbarToggle<CR>', { noremap = true, silent = true })
 
